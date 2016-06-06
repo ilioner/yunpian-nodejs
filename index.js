f889362984275354de91f08317f23c694a681911
TywinZhang <zhangicoding@foxmail.com>
2016-06-06 16:59:00 +0800

    modify

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 var SmsOperator = require("./lib/SmsOperator.jsx");
 var CodeTool = require("./lib/CodeTool.jsx");
 
-var YunPianSDK = function (apikey,mobile) {
+var YunPianSDK = function (apikey,mobile,text) {
     var codeTool = Object.create(CodeTool);
     this.apikey = apikey;
     this.mobile = mobile;
-    this.text = codeTool.getCode();
+    this.text = text + codeTool.getCode();
     this.sendMsg = function () {
         var codeTool = Object.create(CodeTool);
         var data = {};
